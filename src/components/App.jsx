d3feeffd5636f6c24c5bd98d8b51169a6e26c1ff
Bartosz Rajewicz <szejn3@gmail.com>
2024-01-26 22:54:19 +0100

    added App.jsx imports and props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import React from "react";
+import User from './profile/Profile';
+import Statistic from './statistic/Statistic';
+import Friends from  './friends/Friends';
+import Transaction from './transaction/Transaction';
+import user from  './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
+import transaction from './data/transactions.json';
+
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+    <User
+    avatar={user.avatar}
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    stats={user.stats}
+    />
+    
+    
+    </>
   );
 };
+
+export default App;
