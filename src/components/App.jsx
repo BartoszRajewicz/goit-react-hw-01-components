11ca4d204f852e990f58fe4037e8e9129f6d0ae5
Bartosz Rajewicz <szejn3@gmail.com>
2024-01-27 19:34:27 +0100

    checks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import User from './profile/Profile';
-import Statistic from './statistic/Statistic';
-import Friends from  './friends/Friends';
-import Transaction from './transaction/Transaction';
 import user from  './data/user.json';
-import data from './data/data.json';
-import friends from './data/friends.json';
-import transaction from './data/transactions.json';
+
+
 
 
 
