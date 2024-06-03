0b1876acf87fdc2a240c267e360af5b77293c904
Bartosz Rajewicz <szejn3@gmail.com>
2024-06-03 20:55:36 +0200

    added transaction,jsx code

--- src/components/friends/Friends.jsx
+++ src/components/friends/Friends.jsx
@@ -4,19 +4,17 @@ import css from './Friends.module.css';
 
 
 
-
-const FriendList = ({ friends}) => {
-    const element = friends.map(({ avatar, id, name, isOnline}) => (
-<FriendListItem
-key={id}
-id={id}
-isOnline={isOnline}
-name={name}
-avatar={avatar}
-/>
-));
-return <ul className={css.friendList}>{element}</ul>;
-};
+function FriendList({ friends }) {
+    const element = friends.map(({ avatar, id, name, isOnline }) => (
+        <FriendListItem
+            key={id}
+            id={id}
+            isOnline={isOnline}
+            name={name}
+            avatar={avatar} />
+    ));
+    return <ul className={css.friendList}>{element}</ul>;
+}
 
 FriendList.propTypes = {
     friends: PropTypes.arrayOf(
